9a1e513eee8d00875c1c6743e0315a7a1e001b4d
iATKOS <80854153+iatkosx86@users.noreply.github.com>
2021-09-14 12:03:52 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const version = "0.6.20";
+const cacheName = `whackd-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/p/debate.html`,
+        `/p/subscribe.html`,
+        `/p/explore.html`,
+        `/scripts/comlink.global.js`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});

8ea1715b3180c61dc4b8711889ebd72312320043
iATKOS <80854153+iatkosx86@users.noreply.github.com>
2021-09-14 14:32:58 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,20 @@
-const version = "0.6.21";
-const cacheName = `whackd-${version}`;
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        `/`,
-        `/p/debate.html`,
-        `/p/subscribe.html`,
-        `/p/explore.html`
-      ])
-          .then(() => self.skipWaiting());
-    })
-  );
-});
+const js = `
+'use strict';
+const CACHE_VERSION=1;
+let CURRENT_CACHES={offline:"offline-v1"};
+const OFFLINE_URL="/p/debate.html";
+function createCacheBustedRequest(a){let b=new Request(a,{cache:"reload"});if("cache"in b)return b;let c=new URL(a,self.location.href);return c.search+=(c.search?"&":"")+"cachebust="+Date.now(),new Request(c)}self.addEventListener("install",a=>{a.waitUntil(fetch(createCacheBustedRequest(OFFLINE_URL)).then(function(a){return caches.open(CURRENT_CACHES.offline).then(function(b){return b.put(OFFLINE_URL,a)})}))}),self.addEventListener("activate",a=>{let b=Object.keys(CURRENT_CACHES).map(function(a){return CURRENT_CACHES[a]});a.waitUntil(caches.keys().then(a=>Promise.all(a.map(a=>{if(-1===b.indexOf(a))return console.log("Deleting out of date cache:",a),caches.delete(a)}))))}),self.addEventListener("fetch",a=>{("navigate"===a.request.mode||"GET"===a.request.method&&a.request.headers.get("accept").includes("text/html"))&&(console.log("Handling fetch event for",a.request.url),a.respondWith(fetch(a.request).catch(a=>(console.log("Fetch failed; returning offline page instead.",a),caches.match(OFFLINE_URL)))))});
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
+`
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+async function handleRequest(request) {
+  return new Response(js, {
+    headers: {
+      "content-type": "application/javascript;charset=UTF-8",
+    },
+  })
+}
+
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest(event.request))
+})
